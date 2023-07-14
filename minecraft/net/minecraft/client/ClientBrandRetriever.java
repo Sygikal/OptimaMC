be0d465d8c4ee995206beeb9c726bb92c405713d
Sygikal <65551727+Sygikal@users.noreply.github.com>
2023-07-14 16:16:12 -0700

    Revert test omg i hate .patch files

--- minecraft/net/minecraft/client/ClientBrandRetriever.java
+++ minecraft/net/minecraft/client/ClientBrandRetriever.java
@@ -4,6 +4,6 @@ public class ClientBrandRetriever
 {
     public static String getClientModName()
     {
-        return net.minecraftforge.fml.common.FMLCommonHandler.instance().getModName();
+        return "vanilla";
     }
 }
